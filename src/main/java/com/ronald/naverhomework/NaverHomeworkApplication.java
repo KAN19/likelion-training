7163ef4af74f85dc170e2e552b8d6c76d38a5898
Kiet Nguyen <6393445-19520666NK@users.noreply.gitlab.com>
2022-08-25 13:48:10 +0700

    first commit

---
+++ src/main/java/com/ronald/naverhomework/NaverHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.ronald.naverhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaverHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ronald/naverhomework/NaverHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronald.naverhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
